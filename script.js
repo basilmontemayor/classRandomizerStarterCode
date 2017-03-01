79b5cb4e2cf883b938d1518e132663dc086d9d30
Basil Montemayor <basil.l.montemayor@gmail.com>
2017-03-01 23:36:07 +0000

    Buttons

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
+var code2collegeEducators = ["Samuel" , "Sam" , "Trey" , "Matt"];
 
 
+var code2collegeStudents = ["Alexis", "Abi" , "Isaac" , "Evila" ,"Spencer" , "Garret" , "George" , "Tim", "Richard" , "Adrian" , "Thomas"];
 
+$("#teacherButton").click(function(){
+    $("#teacherDisplay").html(code2collegeEducators [Math.floor(Math.random()*code2collegeEducators.length)]);
+
+});
+
+$("#studentButton").click(function(){
+   $("#studentDisplay").html(code2collegeStudents [Math.floor(Math.random()*code2collegeStudents.length)]);
+});
+
+$("#submitastudent").click(function(){
+    code2collegeStudents.push($("#pickastudent").val());
+});
+
+$("#submitateacher").click(function(){
+    code2collegeEducators.push($("#pickateacher").val());
 });
